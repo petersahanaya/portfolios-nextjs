995d2e4ae3f39254c83a3ba83484578b54dfc411
petersahanaya <petersahanaya09@gmail.com>
2023-06-22 20:16:10 +0900

    adding icon and metadata.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,22 @@
-import RouteAnimate from "@component/route";
 import "./globals.css";
 import Header from "@component/header";
+import { Metadata } from "next";
 import { Montserrat } from "next/font/google";
+
+export const metadata: Metadata = {
+  title: "Peter Sahanaya - Front End Dev",
+  description:
+    "Welcome to my personal portfolio website. I am a skilled and experienced front-end developer with a passion for creating visually stunning and user-friendly websites and applications. Browse my work and contact me to discuss your next project.",
+  keywords: ["Next JS", "Three JS", "Portfolio"],
+  creator: "Peter Sahanaya",
+  authors: [
+    {
+      name: "Peter Sahanaya",
+      url: "https://linkedin.com/in/peter-sahanaya",
+    },
+  ],
+  icons : ""
+};
 
 const antonio = Montserrat({
   subsets: ["latin"],
